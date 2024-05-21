83f74c2e4554be78728153835893011c9bb30292
Sepideh <sepideh.golshani72@gmail.com>
2024-05-21 17:15:49 +0200

    TEST

---
+++ Untitled.R
@@ -0,0 +1,7 @@
+##TEST FOR GIT
+
+
+read.csv("/Users/sepidehgolshani/Desktop/Coding_ExNi/main_SOIL.csv")
+main_plant <- read.csv("/Users/sepidehgolshani/Desktop/Coding_ExNi/main_PLANT.csv")
+ssss
+HHH
